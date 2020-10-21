26b5f4609c859e9d93696c162ebeca21050ffaf1
Nishant Patwardhan <nishantpatwardhan@berkeley.edu>
2020-10-20 22:41:27 -0700

    wrapper

--- squadss/actual/main.py
+++
@@ -1,8 +0,0 @@
-import time
-from datetime import datetime
-t_end = time.time() + 60 * 2
-while time.time() < t_end:
-	now = datetime.now()
-	current_time = now.strftime("%H:%M:%S")
-	print(current_time)
-	time.sleep(5)
---
+++ squadss/wrapper.py
@@ -0,0 +1,39 @@
+import time
+from searchEngine import *      # or import whatever function you're using for pulling tweets
+from datetime import datetime
+
+
+#RIP abstraction :(((
+
+keywords = [] # list of keywords, idk what it's supposed to be now
+tweets = {} # dictionary of tweets, idk what it's supposed to be now 
+
+"""
+you can change these
+"""
+total_time = 60*10          # time to run whole script, in seconds
+re_multiSearch_time = 60*2  # time until you call multiSearch again, in seconds
+re_pull_time = 5            # time until you pull tweets from multiSearch, in seconds
+
+
+
+
+t_end_all = time.time() + total_time
+
+def search():
+    if time.time() > t_end_all:
+        exit()
+    print("multiSearch")
+    #multiSearch(keywords)
+    return pull()
+
+
+def pull():
+    t_end = time.time() + re_multiSearch_time
+    while time.time() < t_end:
+        #pullTweets(numTweets)
+        print("pullTweets")
+        time.sleep(re_pull_time)
+    return search()
+
+search()
