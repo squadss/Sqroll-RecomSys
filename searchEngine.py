b9bd8ee020b55ca8daea49bef2b58cd11c9c9c5f
Andrew Zhao <andrewzhao@berkeley.edu>
2020-10-20 23:01:06 -0700

    updated searchengine

--- searchEngine.py
+++ searchEngine.py
@@ -1,35 +1,30 @@
-from topic_stream import get_tweet
+from tweepy_test import search_tweet
 import random
 tweets = {}
-numPerCat = 1
-keywords = ["basketball", "anime"]
+tweetsPerCat = 20
 
 
-def multiSearch(keywords):
-    print("STARTED multi")
-    for word in keywords:
-        tweets[word] = []
-        generator = get_tweet(word)
-        print("finished generator")
-        for _ in range(numPerCat):
-            print("tweet")
-            tweets[word].append(next(generator)) 
-    print("success")
+def multiSearch(categories):
+    for key in categories:
+        tweets[key] = search_tweet(key)
 
 
-def pullTweets(numTweets):
+def pullTweets(numPull):
     results = []
-    randoms = random.sample(range(1, numPerCat), numTweets)
-    for word in tweets:
-        values = tweets[word]
-        for i in randoms:
-            results.append(values[i])
-    print(results)
-    return results
+    for values in tweets.values():
+        randoms = random.sample(values, numPull)
+        results.extend(randoms)
+    return random.sample(results, len(results))
+
 
 def main():
-    multiSearch(keywords)
-    pullTweets(1)
+    multiSearch(["sports", "entertainment"])
+    for i in pullTweets(3):
+        print(i.text)
+        print("\n")
+        print("\n")
+
+
 
 if __name__ == "__main__":
     main()
