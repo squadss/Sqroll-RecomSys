f850bb37a7bd572d6c7b7ff0e427aa8a8894dcd9
Andrew Zhao <andrewzhao@berkeley.edu>
2020-11-08 23:27:51 -0800

    added

--- tweepytest.py
+++ tweepytest.py
@@ -20,18 +20,8 @@ class MyStreamListener(tweepy.StreamListener):
 myStreamListener = MyStreamListener()
 myStream = tweepy.Stream(auth = api.auth, listener=myStreamListener)
 
-print(type(myStream.filter(track=['python'])))
+print(myStream.filter(track=['marvel']))
 
 #print(myStream.filter(follow=["2211149702"]))
 
-print("\n\n\n\n YESSIR")
-
-try:
-    print("Start streaming.")
-    myStream.sample(languages=["en"])
-except KeyboardInterrupt:
-    print("Stopped.")
-finally:
-    print("Done.")
-    myStream.disconnect()
    
