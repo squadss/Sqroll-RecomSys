bd3f9299d94137b3a69c3648e6c5489dcdf39d38
Nishant Patwardhan <nishantpatwardhan@berkeley.edu>
2020-11-10 21:37:22 -0800

    bases amirite

---
+++ squadss/art_base_search.py
@@ -0,0 +1,15 @@
+artKeywords = ["art", "modern art", "renaissance art","contemporary art"]
+artUsers = {
+			"deviantart" : "1239671",
+			"artsy" : "81783051",
+			"itsnicethat" : "17896874",
+			"thedailyheller" : "82387834",
+			"fastcodesign" : "158865339",
+			"opusart" : "798494229920153600",
+			"gagosian" : "32137602",
+			"steveespopowers" : "55268835",
+			"aiww" : "43654274",
+			"osgemeos" : "85857432",
+			"bennygold" : "16676450"
+			"obeygiant" : "17225211"
+			}
