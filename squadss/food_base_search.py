e0ce2352efcf3e7b24b1556bf9caba2f40edd050
Nishant Patwardhan <nishantpatwardhan@berkeley.edu>
2020-11-13 15:01:19 -0800

    fixed dict syntax error

--- squadss/food_base_search.py
+++ squadss/food_base_search.py
@@ -1,13 +1,13 @@
 foodKeywords = ["food", "food network", "master chef", "food travel", "michelin star", "gordon ramsay", "recipe", "weeknight meal", "meal", "dessert", "cheat meal", "food satisfying"]
 foodUsers = {
-			"bonappetit" : "25170188",
-			"natgeofood" : "3366161116",
-			"twitterfood" : "3015271772",
-			"fooddotcom" : "18913121",
-			"newfoodeconomy" : "1167487605925011456",
-			"realcarrotfacts" : " 1074544400",
-			"nytfood" : " 1775731",
-			"civileats" : " 185809250",
-			"iowagirleats" : "29724467",
-			"spoonuniversity" : "1451713728"
+			"bonappetit": "25170188",
+			"natgeofood": "3366161116",
+			"twitterfood": "3015271772",
+			"fooddotcom": "18913121",
+			"newfoodeconomy": "1167487605925011456",
+			"realcarrotfacts": " 1074544400",
+			"nytfood": " 1775731",
+			"civileats": " 185809250",
+			"iowagirleats": "29724467",
+			"spoonuniversity": "1451713728"
 			}
--- squadss/sports_base_search.py
+++ squadss/sports_base_search.py
@@ -1,11 +1,12 @@
 sportsKeywords = ["basketball", "football", "soccer", "baseball", "hockey", "tennis", "NBA", "NFL", "MLB", "NHL", "Champions League", "Premier League", "La Liga", "LeBron", "Ronaldo", "Messi", "Curry", "Brady", "Harden", "Neymar"]
-sportsUsers = {"wojespn" : "50323173",
-				"shamscharania" : "178580925",
-				"NBA" : "19923144",
-				"adamschefter" : "51263592",
-				"NFL" : "19426551",
-				"NFL_memes" : " 492566290",
-				"SportsCenter" : "26257166",
-				"espn" : "2557521",
-				"HoHighlights" : "363183318"
+sportsUsers = {
+				"wojespn": "50323173",
+				"shamscharania": "178580925",
+				"NBA": "19923144",
+				"adamschefter": "51263592",
+				"NFL": "19426551",
+				"NFL_memes": " 492566290",
+				"SportsCenter": "26257166",
+				"espn": "2557521",
+				"HoHighlights": "363183318"
 				}
