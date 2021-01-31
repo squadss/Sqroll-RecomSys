9ff64020805da83370a0bc20e637f4f8f2556998
Andrew Zhao <andrewzhao@berkeley.edu>
2021-01-30 22:02:28 -0800

    edits

--- TEST_newsapi.py
+++ TEST_newsapi.py
@@ -4,9 +4,40 @@ from newsapi import NewsApiClient
 newsapi = NewsApiClient(api_key='a89be3df1f42495ea5af3172cb83be03')
 
 
-x = newsapi.get_top_headlines(sources='bbc-news, the-verge, cnn, fox-news')
-print(x)
+x = newsapi.get_top_headlines(q = "trump", country = 'us', language = 'en', page_size = 50)
+print(len(x["articles"]))
 
+
+news = "abc-news, al-jazeera-english, associated-press, bleacher-report, bloomberg, business-insider, buzzfeed, cnn, cbs-news," \
+        + "espn, fortune,fox-news,fox-sports,google-news, IGN, medical-news-today, national-geographic, nbc-news, politico, reuters," \
+        + "techcrunch, the-verge, the-wall-street-journal, the-washington-post, time, vice-news, wired"
+
+news1 = "espn, abc-news, associated-press, bloomberg, al-jazeera-english, business-insider, buzzfeed, IGN, politico"
+
+news2 = "fox-sports, cnn, cbs-news, fortune, fox-news, google-news, nbc-news, medical-news-today, national-geographic"
+
+news3 = "bleacher-report, reuters, techcrunch, the-verge, the-wall-street-journal, the-washington-post, time, vice-news, wired"
+
+y = newsapi.get_everything(sources = news1, from_param = "2021-01-31", 
+                            language = 'en', sort_by='relevancy', page_size = 100)
+print(len(y["articles"]))
+
+#print(y["articles"])
+
+print(y["articles"][0])
+
+print([i["source"] for i in y["articles"]])
+
+
+
+
+
+"""
+for i in x["articles"]:
+    print(i)
+    print("\n\n\n\n\n\n\n\n")
+
+"""
 """
 # /v2/top-headlines
 top_headlines = newsapi.get_top_headlines(q='bitcoin',
--- tweetsearch.py
+++ tweetsearch.py
@@ -6,14 +6,18 @@ auth = tweepy.OAuthHandler(twitter_credentials.consumer_key, twitter_credentials
 auth.set_access_token(twitter_credentials.access_token, twitter_credentials.access_token_secret)
 api = tweepy.API(auth)
 #print(api.search(q = "*", lang = "en", count = 10, result_type = "popular")[0]._json["entities"]["urls"][0]["url"])
-tweets = api.search(q = "basketball", lang = "en", count = 100, result_type = "mixed")
+#tweets = api.search(q = "basketball", lang = "en", count = 100, result_type = "mixed")
 
 
+"""
 storage = []
 for i in tweets:
     storage.append(i._json["text"])
 
 print(len(storage))
 print(storage[7])
-
-#print(api.trends_place(id = 2459115))
+"""
+lat = 37.2638
+lon = -122.023
+woeid = api.trends_closest(lat, lon)[0]['woeid']
+print(api.trends_place(woeid))
