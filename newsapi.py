984572457854d03f4ccb3599b35ff17f43a4a207
nishantpatwardhan <70258400+nishantpatwardhan@users.noreply.github.com>
2021-02-16 21:57:08 -0800

    Add files via upload
    git guud

--- newsapi.py
+++ newsapi.py
@@ -2,7 +2,20 @@ from newsapi import NewsApiClient
 from datetime import datetime
 import sys
 
-def first():
+'''
+These are 3 functions that return json files with 100 news articles. These functions should be called every 45 minutes, 15 minutes apart.
+news_api_first() should be called, then 15 minutes later news_api_second() should be called and after 15 more minutes news_api_third() should be called.
+15 minutes later, the cycle should repeat forever. In pseudocode:
+while():
+	news_api_first()
+	15 minutes
+	second()
+	news_api_15 minutes
+	news_api_third()
+	15 minutes
+'''
+
+def news_api_first():
 
 	newsapi = NewsApiClient(api_key='a89be3df1f42495ea5af3172cb83be03')
 	news1 = "espn, abc-news, associated-press, bloomberg, al-jazeera-english, business-insider, buzzfeed, IGN, politico"
@@ -12,7 +25,7 @@ def first():
 
 	return json
 
-def second():
+def news_api_second():
 
 	newsapi = NewsApiClient(api_key='a89be3df1f42495ea5af3172cb83be03')
 	news2 = "fox-sports, cnn, cbs-news, fortune, fox-news, google-news, nbc-news, medical-news-today, national-geographic"
@@ -22,7 +35,7 @@ def second():
 
 	return json
 
-def third():
+def news_api_third():
 
 	newsapi = NewsApiClient(api_key='a89be3df1f42495ea5af3172cb83be03')
 	news3 = "bleacher-report, reuters, techcrunch, the-verge, the-wall-street-journal, the-washington-post, time, vice-news, wired"
